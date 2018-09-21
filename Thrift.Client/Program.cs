a90759ab08571a6ec505375d20cb5912f61c3dfc
shuyizhi <shuyizhi@zxisl.com>
2018-09-21 17:13:59 +0800

    Thrift 测试

--- Thrift.Client/Program.cs
+++ Thrift.Client/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelloThrift.Interface;
+using Thrift.Protocol;
+using Thrift.Transport;
 
 namespace Thrift.Client
 {
     class Program
     {
+        private const int SERVERPORT = 8090;
         static void Main( string[] args )
         {
+            try
+            {
+                //设置服务端端口号和地址
+                TTransport transport = new TSocket("localhost", SERVERPORT);
+                transport.Open();
+                //设置传输协议为二进制传输协议
+                TProtocol protocol = new TBinaryProtocol(transport);
+                //创建客户端对象
+                HelloService.Client client = new HelloService.Client(protocol);
+                //调用服务端的方法
+                Console.WriteLine(client.HelloString("HelloThrift"));
+                Console.ReadKey();
+            }
+            catch (TTransportException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+
+            }
         }
     }
 }
--- Thrift.Server/Program.cs
+++ Thrift.Server/Program.cs
@@ -7,22 +7,34 @@ using Thrift;
 using Thrift.Protocol;
 using Thrift.Transport;
 using Thrift.Server;
+using HelloThrift.Interface;
 
 namespace Thrift.Server
 {
     class Program
     {
-        private const int SERVERPORT = 8080;
+        private const int SERVERPORT = 8090;
         static void Main( string[] args )
         {
-            /**设置服务端口*/
-            TServerSocket serverTransport = new TServerSocket(SERVERPORT);
-            /**设置传输协议工厂*/
-            TBinaryProtocol.Factory factory = new TBinaryProtocol.Factory();
-            /**关联处理器与服务的实现*/
-            TProcessor processor = new HelloThrift.Interface.HelloService.Processor(new MyService());
-            /**创建服务端对象*/
-            TServer server = new TThreadPoolServer(processor, serverTransport);
+            try
+            {
+                //设置服务端口为8080
+                TServerSocket serverTransport = new TServerSocket(SERVERPORT);
+                //设置传输协议工厂
+                TBinaryProtocol.Factory factory = new TBinaryProtocol.Factory();
+                //关联处理器与服务的实现
+                TProcessor processor = new HelloService.Processor(new MyService());
+                //创建服务端对象
+                TServer server = new TThreadPoolServer(processor, serverTransport, new TTransportFactory(), factory);
+                //TServer server = new TSimpleServer(processor, serverTransport);
+                Console.WriteLine("服务端正在监听" + SERVERPORT + "端口");
+                server.Serve();
+            }
+            catch (TTransportException ex)//捕获异常信息
+            {
+                //打印异常信息
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
