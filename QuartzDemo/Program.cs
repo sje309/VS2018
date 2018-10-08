b801beafe6b94cae4d2e927fddaedb9a6803e35f
shuyizhi <shuyizhi@zxisl.com>
2018-10-08 17:31:23 +0800

    Quartz.Net

---
+++ QuartzDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuartzDemo
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+            //Topshelf.HostFactory.Run(x =>
+            //{
+            //    x.UseLog4Net();
+            //    x.ser
+            //});
+
+        }
+    }
+}
